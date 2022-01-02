55237cea858366a49283a9ef3e63738e5ac2c449
Aryan Dubey <66486350+AD2206@users.noreply.github.com>
2022-01-02 13:30:52 +0530

    Add files via upload

---
+++ oddEven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+
+int number;
+
+printf("Enter a number \n");
+scanf("%d", &number);
+
+if(number % 2 == 0)
+    printf("Its and Even Number \n");
+else
+    printf("Its an Odd Number \n");
+
+}
